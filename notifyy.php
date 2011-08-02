0fd4a97b4224428eb2e7d558f41351b8c106e107
Andy Soell <andy@teamsoell.com>
2011-08-02 15:27:43 -0400

    Fleshed out with more options
    * Added plugin parameters  * Optional message  * Optional email subject
    * Email method changed to use configuration's email settings * Email
    message includes common footer and link to the administrator

--- notifyy.php
+++ notifyy.php
@@ -5,10 +5,10 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.plugin.plugin' );
 
 class plgContentNotifyy extends JPlugin {
-    public $recipients;
+    public $params;
     
     function plgContentNotifyy( &$subject, $params) {
-        $this->recipients = $params;
+        $this->params = $params;
         parent::__construct( $subject, $params );
     }
     
@@ -23,11 +23,34 @@ class plgContentNotifyy extends JPlugin {
             $query->from('#__users as user INNER JOIN #__user_usergroup_map as usergroup ON user.id=usergroup.user_id');
             $query->where('usergroup.group_id IN ('.$groups.')');
             $db->setQuery($query);  
-            
-            if (count($db->loadObjectList('email'))>0) {
-                $recipients = array_keys($db->loadObjectList('email'));
-    
-                JUtility::sendMail("genteel@amsoell.com", "The Genteel Article Watch", implode(',',$recipients), "New article submitted", "There's a new article waiting for review!\n\n".print_r($recipients, true));
+            $emails = $db->loadObjectList('email');
+
+            if (count($emails)>0) {
+                $recipients = array_keys($emails);
+                
+                $mailer =& JFactory::getMailer();
+                $config =& JFactory::getConfig();
+                $sender = array( 
+                    $config->getValue('config.mailfrom'),
+                    $config->getValue('config.fromname').' - Article Watch' );
+ 
+                $mailer->setSender($sender);
+                $mailer->addRecipient($recipients);
+                
+                $body = $this->params->get("message");
+                $body .= "<p><b>".$article->title."</b></p>";
+                $body .= "<p>You are receiving this message because there is a new article ready to review at ".$config->getValue("config.sitename").". Visit ".JRoute::_(JURI::base().'index.php?option=com_content&view=articles')." to review it. If you would like to be removed from these notifications, please contact your administrator: <a href=\"mailto:".$config->getValue('config.mailfrom')."\">".$config->getValue('config.mailfrom')."</a></p>";                
+                
+                $mailer->isHTML(true);
+                $mailer->Encoding = 'base64';
+                $mailer->setBody($body);                
+                $mailer->setSubject($this->params->get("subject"));
+                
+                $send =& $mailer->Send();
+                
+                if ($send!==true) {
+                    die("Could not send mail: " & $send->message);
+                }
             }
         }
 
